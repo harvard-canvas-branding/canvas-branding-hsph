b4e78e35a755e99651e121c4930141bb07715667
David Marshall <dpegasusm@gmail.com>
2019-04-23 16:18:22 -0400

    Include and load Jquery info and jquery UI

--- js/canvas.js
+++ js/canvas.js
@@ -1,3 +1,10 @@
+/**
+ * This file was written by the HSPH WebTeam for use on HSPH related Canvas Courses. 
+ * DO NOT modify this file in any way without express written permssion from the 
+ * HSPH webteam. Permission can be obtained by emailing webteam@hsph.harvard.edu
+ */
+
+ // Include Google Analytics code for page tracking.
 (function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
 (i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
 m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
@@ -8,69 +15,108 @@ ga('require', 'displayfeatures');
 ga('require', 'linkid', 'linkid.js');
 ga('send', 'pageview');
 
-//Load jQuery library using plain JavaScript
-(function(){
-	var kscript = document.createElement('script');
-	kscript.type = 'text/javascript';
-	kscript.src = 'https://cdnapisec.kaltura.com/p/1446471/sp/144647100/embedIframeJs/uiconf_id/30101351/partner_id/1446471';
-	document.getElementsByTagName('head')[0].appendChild(kscript);
+/**
+ * Script loader function.
+ * 
+ * @param string src Script source URL to be loaded.
+ */ 
+function loadScript(src) {
+    return new Promise(function (resolve, reject) {
+        var s;
+        s = document.createElement('script');
+        s.src = src;
+        s.onload = resolve;
+        s.onerror = reject;
+        document.head.appendChild(s);
+    });
+}
 
-	// Poll for jQuery to come into existance
-	var checkReady = function(callback) {
-		if ( window.jQuery && $( ".kaltura-threeplay" ).length && typeof kWidget !== 'undefined' ) {
-			callback(jQuery);
+// add the akltura script to the head of our document.
+loadScript( 'https://cdnapisec.kaltura.com/p/1446471/sp/144647100/embedIframeJs/uiconf_id/30101351/partner_id/1446471' );
+loadScript( 'https://code.jquery.com/ui/1.12.1/jquery-ui.min.js' );
+
+// Poll for jQuery to come into existance.
+var checkReady = function(callback) {
+	if ( window.jQuery && $( ".kaltura-threeplay" ).length && typeof kWidget !== 'undefined' ) {
+		callback(jQuery);
+	} else {
+		window.setTimeout(function() { checkReady(callback); }, 100);
+	}
+};
+
+// jQuery Has been loaded, we can now run the scripts.
+checkReady( function($) {
+	// Jquery has been loaded so we can now use it.
+	$( '.kaltura-threeplay' ).each(function( index ) {
+
+		// lets see if we have a kaltura attribute
+		if ( $( this ).attr( "id" ) ) {
+			$( this ).replaceWith( '<div class="video" id="video' + index + '"><div class="kaltura-player-container"><!--  maintain 16/9 aspect ratio: --><div class="kaltura-player-container-absolute"><div id="kaltura_player" style="width:712px; height: 401px;"></div></div></div></div>' );
+
+			// Kwidget Embed code to load the kaltura player.
+			mw.setConfig('EmbedPlayer.EnableIpadHTMLControls', false);
+			mw.setConfig('EmbedPlayer.WebKitPlaysInline', true);
+			kWidget.embed({
+				'targetId' : 'kaltura_player',
+				'flashvars':{ // flashvars allows you to set runtime uiVar configuration overrides.
+					'autoPlay': false
+				},
+				'wid': '_1446471',
+				'uiconf_id' : '30101351',
+				'entry_id' : $( this ).attr( "id" )
+			});
 		} else {
-			window.setTimeout(function() { checkReady(callback); }, 100);
+			$( this ).replaceWith( '<div class="video" id="video' + index + '"></div>' );
 		}
-	};
 
-	// Start polling...
-	checkReady(function($) {
-		$( '.kaltura-threeplay' ).each(function( index ) {
-
-			// lets see if we have a kaltura attribute
-			if ( $( this ).attr( "id" ) ) {
-				$( this ).replaceWith( '<div class="video" id="video' + index + '"><div class="kaltura-player-container"><!--  maintain 16/9 aspect ratio: --><div class="kaltura-player-container-absolute"><div id="kaltura_player" style="width:712px; height: 401px;"></div></div></div></div>' );
-
-				mw.setConfig('EmbedPlayer.EnableIpadHTMLControls', false);
-				mw.setConfig('EmbedPlayer.WebKitPlaysInline', true);
-				kWidget.embed({
-					'targetId' : 'kaltura_player',
-					'flashvars':{ // flashvars allows you to set runtime uiVar configuration overrides.
-						'autoPlay': false
-					},
-					'wid': '_1446471',
-					'uiconf_id' : '30101351',
-					'entry_id' : $( this ).attr( "id" )
-				});
-			} else {
-				$( this ).replaceWith( '<div class="video" id="video' + index + '"></div>' );
+		// Now that we have loaded the kaltura player we can also load threeplay. 
+		if ( $( this ).attr( "title" ) ) {
+			p3_api_key = "";
+			p3_window_wait = false;
+			$( '#video' + index ).append( '<div id="transcript_' + $( this ).attr( "title" ) + '"></div>');
+			if (typeof p3_instances == "undefined") p3_instances = {};
+			if (!p3_instances["kaltura_player"]){
+				p3_instances["kaltura_player"] = {
+				file_id: $( this ).attr( "title" ),
+				player_type: "kaltura",
+				api_version: "simple",
+				project_id: "11439",
+				platform_integration: false  }
 			}
-
-			// how about a threeplay one?
-			if ( $( this ).attr( "title" ) ) {
-				p3_api_key = "";
-				p3_window_wait = false;
-				$( '#video' + index ).append( '<div id="transcript_' + $( this ).attr( "title" ) + '"></div>');
-				if (typeof p3_instances == "undefined") p3_instances = {};
-				if (!p3_instances["kaltura_player"]){
-					p3_instances["kaltura_player"] = {
-					file_id: $( this ).attr( "title" ),
-					player_type: "kaltura",
-					api_version: "simple",
-					project_id: "11439",
-					platform_integration: false  }
-				}
-				p3_instances["kaltura_player"]["transcript"] = {target:"transcript_" + $( this ).attr( "title" ),width:"712",height:"290",skin:"minimalist",can_collapse:true,progressive_tracking:true,can_print:true,can_download:true,download_format:"pdf"}
-				if (typeof p3_is_loading == "undefined" || (!p3_is_loading)) {
-					p3_is_loading = true;
-					$( '#video' + index ).append( '<div id="p3-js-main-root"></div>' );
-					var e = document.createElement('script');
-					e.async = true;
-					e.src = "//p3.3playmedia.com/p3.js"
-					document.getElementById('p3-js-main-root').appendChild(e);
-				}
+			// attach the threeplay player to the Kaltura player. 
+			p3_instances["kaltura_player"]["transcript"] = {target:"transcript_" + $( this ).attr( "title" ),width:"712",height:"290",skin:"minimalist",can_collapse:true,progressive_tracking:true,can_print:true,can_download:true,download_format:"pdf"}
+			if (typeof p3_is_loading == "undefined" || (!p3_is_loading)) {
+				p3_is_loading = true;
+				$( '#video' + index ).append( '<div id="p3-js-main-root"></div>' );
+				var e = document.createElement('script');
+				e.async = true;
+				e.src = "//p3.3playmedia.com/p3.js"
+				document.getElementById('p3-js-main-root').appendChild(e);
 			}
-		});
+		}
 	});
-})();
+});
+
+// now lets look at jQuery UI it needs to be loaded and load complete.
+function checkJqueryUI() {
+	if (typeof jQuery.ui != 'undefined') {
+		do_jqueryui();
+	} else {
+		window.setTimeout( checkJqueryUI, 50 );
+	}
+}
+
+// jQuery UI has loaded lets run our scripts.
+function do_jqueryui() {
+	// Check that we are ready to go.
+	jQuery( document ).ready(function($) {
+		// Activate Tabs
+		$('#tabs').tabs();
+
+		// Activate Accordions.
+		$('.accordion-custom').accordion({heightStyle: "content"});
+	});
+}
+
+// run the jquery UI checks.
+checkJqueryUI();
