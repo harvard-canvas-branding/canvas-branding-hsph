14f9cf4015966144751b17467e657381dc5ddc08
David Marshall <dpegasusm@gmail.com>
2019-05-03 14:54:06 -0400

    some webpack stuff

--- webpack.config.js
+++ webpack.config.js
@@ -19,10 +19,12 @@ module.exports = {
         }
       },
       {
+        enforce: 'pre',
         test: /\.js$/,
         exclude: /node_modules/,
-        use: {
-          loader: 'eslint-loader'
+        loader: 'eslint-loader',
+        options: {
+          fix: true
         }
       }
     ]
