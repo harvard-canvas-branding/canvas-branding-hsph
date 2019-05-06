17b2d970513a5fbfff08505f6a89ab9ae7c85978
David Marshall <dpegasusm@gmail.com>
2019-05-06 15:45:48 -0400

    Load File

---
+++ js/load.js
@@ -0,0 +1,5 @@
+var hsphCanvas = document.createElement('script');
+hsphCanvas.type = 'text/javascript';
+hsphCanvas.async = true;
+hsphCanvas.src = 'https://s3.amazonaws.com/hsph-canvas/bundle.js';
+document.getElementsByTagName('head')[0].appendChild(hsphCanvas);
--- webpack.config.js
+++ webpack.config.js
@@ -4,6 +4,8 @@ module.exports = {
   entry: './js/index.js',
   output: {
     filename: 'bundle.js',
-    path: path.resolve(__dirname, 'dist')
+    path: path.resolve(__dirname, 'dist'),
+    chunkFilename: "[name].[id].js",
+    filename: "build.[name].js"
   }
 };
