7ac830cf35555676cceaf55f4284788e088c4aaf
David Marshall <dpegasusm@gmail.com>
2019-05-06 15:47:14 -0400

    Canvas

--- js/canvas.js
+++ js/canvas.js
@@ -1,8 +1,3 @@
-/**
- * This file was written by the HSPH WebTeam for use on HSPH related Canvas Courses.
- * DO NOT modify this file in any way without express written permssion from the
- * HSPH webteam. Permission can be obtained by emailing webteam@hsph.harvard.edu
- */
 const hsphJquery = require( "jquery" );
 const hsphJqueryUI = require( "jquery-ui" );
 
--- js/load.js
+++ js/load.js
@@ -1,3 +1,9 @@
+/**
+ * This file was written by the HSPH WebTeam for use on HSPH related Canvas Courses.
+ * DO NOT modify this file in any way without express written permssion from the
+ * HSPH webteam. Permission can be obtained by emailing webteam@hsph.harvard.edu
+ */
+
 var hsphCanvas = document.createElement('script');
 hsphCanvas.type = 'text/javascript';
 hsphCanvas.async = true;
