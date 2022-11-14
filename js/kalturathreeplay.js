7d9caea0200ff96888bbba04b711094e98869074
Charley Gibney <cgibney@hsph.harvard.edu>
2022-11-14 15:33:10 -0500

    Add timeout if kaltura-threeplay div not found

--- js/kalturathreeplay.js
+++ js/kalturathreeplay.js
@@ -2,6 +2,18 @@ function hsphKalturaThreePlay() {
     // Check that we are ready to go.
     jQuery( document ).ready(function($) {
         // Jquery has been loaded so we can now use it.
+        
+        // Check to see whether the kaltura-threeplay div exists yet; if not, stall.
+        if( $(".kaltura-threeplay").length == 0 ) {                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                               
+            setTimeout(function() {
+                console.log('kaltura-threeplay div not found; wait 3 seconds');    
+                if( window.location.href.indexOf( 'harvard.test.instructure.com' ) > -1 ) {
+                    hsphloadScript( 'https://content.sph.harvard.edu/it/canvas-theme/dev/js/manifest.js' );
+                    } else {
+                    hsphloadScript( 'https://content.sph.harvard.edu/it/canvas-theme/prod/js/manifest.js' );
+                    }                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                 
+            }, 3000);}   
+
         $( '.kaltura-threeplay' ).each(function( index ) {
 
             // lets see if we have a kaltura attribute
